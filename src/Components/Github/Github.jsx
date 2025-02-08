0ac3fa61557a3ac9aab1569602066d4bcc718d6a
KrishnaThakur10 <krishnathakur27082005@gmail.com>
2025-02-08 17:35:18 +0530

    creating new commit
    Signed-off-by: KrishnaThakur10 <krishnathakur27082005@gmail.com>

--- src/Components/Github/Github.jsx
+++ src/Components/Github/Github.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
 
 function Github() {
+  const data = useLoaderData()
+  // const[data, setdata] = useState([])
+  // useEffect(()=>{
+  //   fetch("https://api.github.com/users/KrishnaThakur10")
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     setdata(data)
+
+  //   }, [setdata])
+  // })
   return (
-    <div className='text-center text-white bg-gray-600 text-4xl font-bold p-6'>
-        User Followers:
+    <>
+      <div className='text-center text-white bg-gray-600 text-4xl font-bold p-6 rounded'>
+        {data.login} : {data.followers}
+       <img src={data.avatar_url} alt="" width={180} className='rounded-lg'/>
     </div>
+    </>
   )
 }
 
 export default Github
+
+export const githubInfoLoader = async () => {
+  const response = await fetch("https://api.github.com/users/KrishnaThakur10")
+  return response.json()
+}
